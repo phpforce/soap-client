69c7a0dc21f1ba87e6143997a3f5318c72c94b40
David de Boer <david@ddeboer.nl>
2011-12-30 17:05:09 +0100

    Make sure tests support any install directory

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,5 +1,14 @@
 <?php
 
 $loader = require __DIR__. '/../vendor/.composer/autoload.php';
-$loader->add('Ddeboer\Salesforce\ClientBundle', __DIR__ . '/../../../../');
-$loader->register();
+
+spl_autoload_register(function($class) {
+    if (0 === strpos($class, 'Ddeboer\\Salesforce\\ClientBundle\\')) {
+        $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 3)).'.php';
+        if (!stream_resolve_include_path($path)) {
+            return false;
+        }
+        require_once $path;
+        return true;
+    }
+});
