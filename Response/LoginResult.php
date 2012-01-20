3211780c58294fb1a83ed17fe6ff064647089ebd
David de Boer <david@ddeboer.nl>
2012-01-20 15:36:03 +0100

    Add server instance retrieval

--- Response/LoginResult.php
+++ Response/LoginResult.php
@@ -11,4 +11,20 @@ class LoginResult
     public $sessionId;
     public $userId;
     public $userInfo;
+
+    /**
+     * Get the server instance, e.g. ‘eu1-api’
+     *
+     * @return string
+     */
+    public function getServerInstance()
+    {
+        if (null === $this->serverUrl) {
+            throw new \UnexpectedValueException('Server URL must be set');
+        }
+
+        $url = parse_url($this->serverUrl);
+        $host = explode('.', $url['host']);
+        return $host[0];
+    }
 }
