4f3702f4ab3f27c72fc9bc3bc8a30205919d20e0
Etienne Lachance <et@etiennelachance.com>
2013-07-17 14:03:45 -0400

    Override SObject to return N/A if the property don't exist and fall back __call to return the property

--- src/Phpforce/SoapClient/Result/SObject.php
+++ src/Phpforce/SoapClient/Result/SObject.php
@@ -17,4 +17,19 @@ class SObject
     {
         return $this->Id;
     }
+
+    public function __get($prop) {
+        if (property_exists($this, $prop)) {
+            return $this->$prop;
+        } else {
+            return "N/A";
+        }
+    }
+
+    public function __call($method, $args) {
+        if (isset($this->$method) && is_callable($method)) {
+            $closure = $this->$method;
+            call_user_func_array($closure, $args);
+        }
+    }
 }
