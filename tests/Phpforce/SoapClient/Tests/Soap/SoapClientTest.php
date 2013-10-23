0943810b2050c8708bd8ac17d046a44ddc6eab14
Johannes Heinen <johannes.heinen@gmail.com>
2013-10-23 11:48:52 +0200

    Added test for enterprise wsdl namespace detection

--- tests/Phpforce/SoapClient/Tests/Soap/SoapClientTest.php
+++ tests/Phpforce/SoapClient/Tests/Soap/SoapClientTest.php
@@ -5,13 +5,23 @@ use Phpforce\SoapClient\Soap\SoapClient;
 
 class SoapClientTest extends \PHPUnit_Framework_TestCase
 {
-    public function testGetNamespace()
+    public function testGetNamespacePartner()
     {
-        $this->assertEquals('urn:partner.soap.sforce.com', $this->getSoapClient()->getNamespace('tns'));
+        $this->assertEquals('urn:partner.soap.sforce.com', $this->getSoapClientPartner()->getNamespace('tns'));
     }
     
-    protected function getSoapClient()
+    protected function getSoapClientPartner()
     {
         return new SoapClient(__DIR__.'/../Fixtures/sandbox.partner.wsdl.xml');
+    }
+
+    public function testGetNamespaceEnterprise()
+    {
+        $this->assertEquals('urn:enterprise.soap.sforce.com', $this->getSoapClientEnterprise()->getNamespace('tns'));
+    }
+
+    protected function getSoapClientEnterprise()
+    {
+        return new SoapClient(__DIR__.'/../Fixtures/sandbox.enterprise.wsdl.xml');
     }
 }
