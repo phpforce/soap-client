d9327e166cd19ec94f9e14a70b8b752c55ee86d9
Shaun Hardy <shardy@sesamecommunications.com>
2014-06-12 08:40:40 -0700

    Fixed variable scoping issue which can lead to out of memory error.

--- src/Phpforce/SoapClient/Soap/SoapClient.php
+++ src/Phpforce/SoapClient/Soap/SoapClient.php
@@ -26,6 +26,7 @@ class SoapClient extends \SoapClient
 
             $soapTypes = $this->__getTypes();
             foreach ($soapTypes as $soapType) {
+                $properties = array();
                 $lines = explode("\n", $soapType);
                 if (!preg_match('/struct (.*) {/', $lines[0], $matches)) {
                     continue;
